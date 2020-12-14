45184cac3749a30d50064e34be121e4dafe20c8a
Aztec <salgorog@hotmail.com>
2020-12-14 00:01:44 -0700

    script start:dev now working

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,6 @@
-const APP_PREFIX = 'FoodFest-';     
+const APP_PREFIX = 'FoodFest-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE)
-      })
-    )
-  })
-
 const FILES_TO_CACHE = [
     "./index.html",
     "./events.html",
@@ -23,4 +13,13 @@ const FILES_TO_CACHE = [
     "./dist/events.bundle.js",
     "./dist/tickets.bundle.js",
     "./dist/schedule.bundle.js"
-  ];
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
