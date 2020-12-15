7e2bc4c38cb5f45b3e5a68181c0aab920b5f4a42
Aztec <salgorog@hotmail.com>
2020-12-15 16:44:45 -0700

    intercepted fetch requests

--- service-worker.js
+++ service-worker.js
@@ -47,6 +47,14 @@ self.addEventListener('activate', function (e) {
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
-
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
     )
 })
