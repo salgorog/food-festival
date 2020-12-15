a604e74a126e48e1f97c9f017549ecb91cd79fe5
Aztec <salgorog@hotmail.com>
2020-12-14 21:34:20 -0700

    modified service-worker to fetch

--- service-worker.js
+++ service-worker.js
@@ -43,3 +43,10 @@ self.addEventListener('activate', function (e) {
         })
     );
 });
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+
+    )
+})
